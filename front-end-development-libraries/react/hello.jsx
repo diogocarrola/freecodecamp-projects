9a93aa528adb0c1a9e404dc38547f65442747371
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-08 23:49:45 +0100

    Create a Complex JSX Element

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,1 +1,11 @@
-const JSX = <h1>Hello JSX!</h1>;
+const JSX = (
+  <div>
+    <h1>Welcome to My Page</h1>
+    <p>Here are some interesting facts:</p>
+    <ul>
+      <li>React is declarative</li>
+      <li>JSX looks like HTML</li>
+      <li>Components are reusable</li>
+    </ul>
+  </div>
+);
