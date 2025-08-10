a93a0831dc07188161db8c89122c438edeb60c6e
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-10 23:30:22 +0100

    Render HTML Elements to the DOM

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,7 +1,9 @@
 const JSX = (
   <div>
-    <h1>This is a block of JSX</h1>
-    <p>Here's a subtitle</p>
-    {/* This is a JSX comment - it won't appear in the rendered output */}
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
   </div>
 );
+
+// Add your code below this line
+ReactDOM.render(JSX, document.getElementById('challenge-node'));
