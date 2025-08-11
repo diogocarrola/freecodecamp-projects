fc5eb82353754cbeefc78ab0aacf5bedeaec2023
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-11 23:45:46 +0100

    Learn About Self-Closing JSX Tags

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,5 +1,7 @@
 const JSX = (
-  <div className="myDiv">
-    <h1>Add a class to this div</h1>
+  <div>
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
   </div>
 );
