0203f00339c7b07b7b44c6487636c02fe69357ca
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-13 23:02:21 +0100

    Create a Component with Composition

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,14 +1,23 @@
-class MyComponent extends React.Component {
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    // Change code below this line
     return (
       <div>
-        <h1>Hello React!</h1>
+        <h1>I am the parent</h1>
+        { /* Change code below this line */ }
+        <ChildComponent />
+        { /* Change code above this line */ }
       </div>
-    )
-    // Change code above this line
+    );
   }
 };
