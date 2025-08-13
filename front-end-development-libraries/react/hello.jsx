c04c9e701f11cd4c2fa81256bc0f93295d7c05ba
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-13 23:13:02 +0100

    Render a Class Component to the DOM

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,33 +1,19 @@
-class Fruits extends React.Component {
+class TypesOfFood extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        <h2>Fruits:</h2>
-        { /* Change code below this line */ }
-        <NonCitrus />
-        <Citrus />
-        { /* Change code above this line */ }
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        <Fruits />
+        <Vegetables />
+        {/* Change code above this line */}
       </div>
     );
   }
 };
 
-class TypesOfFood extends React.Component {
-  constructor(props) {
-     super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h1>Types of Food:</h1>
-        { /* Change code below this line */ }
-        <Fruits />
-        { /* Change code above this line */ }
-        <Vegetables />
-      </div>
-    );
-  }
-};
+// Change code below this line
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
