5fdc2b1b1bac02471810a75f0bbb05e2ff32f719
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-15 00:00:01 +0100

    Override Default Props

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,9 +1,18 @@
-const ShoppingCart = (props) => {
-  return (
-    <div>
-      <h1>Shopping Cart Component</h1>
-    </div>
-  )
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+}
+
+Items.defaultProps = {
+  quantity: 0
+}
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    { /* Change code below this line */ }
+    return <Items quantity={10} />
+    { /* Change code above this line */ }
+  }
 };
-// Change code below this line
-ShoppingCart.defaultProps = { items: 0 };
