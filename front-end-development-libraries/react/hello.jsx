766cf8f3396f32bafe8402cfafc84098e989c7a9
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-20 23:41:29 +0100

    Create a Controlled Input

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,41 +1,31 @@
-class Counter extends React.Component {
+class ControlledInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      input: ''
     };
     // Change code below this line
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     // Change code above this line
   }
   // Change code below this line
-  increment() {
-    this.setState(state => ({
-      count: state.count + 1
-    }));
-  }
-
-  decrement() {
-    this.setState(state => ({
-      count: state.count - 1
-    }));
-  }
-
-  reset() {
+  handleChange(event) {
     this.setState({
-      count: 0
+      input: event.target.value
     });
   }
   // Change code above this line
   render() {
     return (
       <div>
-        <button className='inc' onClick={this.increment}>Increment!</button>
-        <button className='dec' onClick={this.decrement}>Decrement!</button>
-        <button className='reset' onClick={this.reset}>Reset</button>
-        <h1>Current Count: {this.state.count}</h1>
+        { /* Change code below this line */}
+        <input 
+          value={this.state.input} 
+          onChange={this.handleChange} 
+        />
+        { /* Change code above this line */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
       </div>
     );
   }
