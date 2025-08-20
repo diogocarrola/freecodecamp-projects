147e7c4231dc488b26bdb2b06534ce68821459e8
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-20 23:46:23 +0100

    Create a Controlled Form

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,32 +1,42 @@
-class ControlledInput extends React.Component {
+class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ''
+      input: '',
+      submit: ''
     };
-    // Change code below this line
     this.handleChange = this.handleChange.bind(this);
-    // Change code above this line
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  // Change code below this line
   handleChange(event) {
     this.setState({
       input: event.target.value
     });
   }
-  // Change code above this line
+  handleSubmit(event) {
+    // Change code below this line
+    event.preventDefault();
+    this.setState({
+      submit: this.state.input
+    });
+    // Change code above this line
+  }
   render() {
     return (
       <div>
-        { /* Change code below this line */}
-        <input 
-          value={this.state.input} 
-          onChange={this.handleChange} 
-        />
-        { /* Change code above this line */}
-        <h4>Controlled Input:</h4>
-        <p>{this.state.input}</p>
+        <form onSubmit={this.handleSubmit}>
+          {/* Change code below this line */}
+          <input 
+            value={this.state.input} 
+            onChange={this.handleChange} 
+          />
+          {/* Change code above this line */}
+          <button type='submit'>Submit!</button>
+        </form>
+        {/* Change code below this line */}
+        <h1>{this.state.submit}</h1>
+        {/* Change code above this line */}
       </div>
     );
   }
-};
+}
