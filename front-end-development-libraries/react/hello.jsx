3c241ff1c3c72fc13db0c83669d855d79635d134
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-21 23:57:55 +0100

    Use the Lifecycle Method componentWillMount

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,57 +1,13 @@
-class MyApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(event) {
-    this.setState({
-      inputValue: event.target.value
-    });
-  }
-  render() {
-    return (
-       <div>
-        { /* Change code below this line */ }
-        <GetInput 
-          input={this.state.inputValue} 
-          handleChange={this.handleChange} 
-        />
-        <RenderInput input={this.state.inputValue} />
-        { /* Change code above this line */ }
-       </div>
-    );
-  }
-};
-
-class GetInput extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
   }
-  render() {
-    return (
-      <div>
-        <h3>Get Input:</h3>
-        <input
-          value={this.props.input}
-          onChange={this.props.handleChange}/>
-      </div>
-    );
-  }
-};
-
-class RenderInput extends React.Component {
-  constructor(props) {
-    super(props);
+  componentWillMount() {
+    // Change code below this line
+    console.log('Component is about to mount');
+    // Change code above this line
   }
   render() {
-    return (
-      <div>
-        <h3>Input Render:</h3>
-        <p>{this.props.input}</p>
-      </div>
-    );
+    return <div />
   }
 };
