97a21b464befddfd5364d043c079a3bbc014b634
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-23 23:38:50 +0100

    Introducing Inline Styles

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,40 +1,7 @@
-class OnlyEvens extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('Should I update?');
-    // Change code below this line
-    return nextProps.value % 2 === 0;
-    // Change code above this line
-  }
-  componentDidUpdate() {
-    console.log('Component re-rendered.');
-  }
-  render() {
-    return <h1>{this.props.value}</h1>;
-  }
-}
-
-class Controller extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0
-    };
-    this.addValue = this.addValue.bind(this);
-  }
-  addValue() {
-    this.setState(state => ({
-      value: state.value + 1
-    }));
-  }
+class Colorful extends React.Component {
   render() {
     return (
-      <div>
-        <button onClick={this.addValue}>Add</button>
-        <OnlyEvens value={this.state.value} />
-      </div>
+      <div style={{color: "red", fontSize: 72}}>Big Red</div>
     );
   }
 };
