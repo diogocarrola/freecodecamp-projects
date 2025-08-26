281839bfc5f380a0f1d4599ce1aa65db47f9b809
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-26 23:04:40 +0100

    Render React on the Server with renderToString

--- front-end-development-libraries/react/hello.jsx
+++ front-end-development-libraries/react/hello.jsx
@@ -1,45 +1,11 @@
-class MyComponent extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      users: [
-        {
-          username: 'Jeff',
-          online: true
-        },
-        {
-          username: 'Alan',
-          online: false
-        },
-        {
-          username: 'Mary',
-          online: true
-        },
-        {
-          username: 'Jim',
-          online: false
-        },
-        {
-          username: 'Sara',
-          online: true
-        },
-        {
-          username: 'Laura',
-          online: true
-        }
-      ]
-    };
   }
   render() {
-    const usersOnline = this.state.users.filter(user => user.online); // Change this line
-    const renderOnline = usersOnline.map((user, index) => (
-      <li key={index}>{user.username}</li>
-    )); // Change this line
-    return (
-      <div>
-        <h1>Current Online Users:</h1>
-        <ul>{renderOnline}</ul>
-      </div>
-    );
+    return <div/>
   }
-}
+};
+
+// Change code below this line
+ReactDOMServer.renderToString(<App />);
