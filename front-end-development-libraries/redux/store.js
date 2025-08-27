f0e213c6cbc17afaf1280ac7590fd1277c5f0ede
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-28 00:12:22 +0100

    Get State from the Redux Store

--- front-end-development-libraries/redux/store.js
+++ front-end-development-libraries/redux/store.js
@@ -1,8 +1,6 @@
-const reducer = (state = 5) => {
-  return state;
-}
+const store = Redux.createStore(
+  (state = 5) => state
+);
 
-// Redux methods are available from a Redux object
-// For example: Redux.createStore()
-// Define the store here:
-const store = Redux.createStore(reducer);
+// Change code below this line
+const currentState = store.getState();
