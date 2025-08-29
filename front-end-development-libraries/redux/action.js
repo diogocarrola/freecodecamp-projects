9ec49c640bca51de4a7fdcf2fdc33ed054f82bde
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-29 23:40:16 +0100

    Dispatch an Action Event

--- front-end-development-libraries/redux/action.js
+++ front-end-development-libraries/redux/action.js
@@ -1,7 +1,12 @@
-const action = {
-  type: 'LOGIN'
-}
-// Define an action creator here:
-function actionCreator() {
-  return action;
-}
+const store = Redux.createStore(
+  (state = {login: false}) => state
+);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+// Dispatch the action here:
+store.dispatch(loginAction());
