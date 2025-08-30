cdcb294cc2d253ce640ed546310901d36f347c1d
Diogo Carrola <diogo.carrola@gmail.com>
2025-08-31 00:07:00 +0100

    Use const for Action Types

--- front-end-development-libraries/redux/action.js
+++ front-end-development-libraries/redux/action.js
@@ -1,34 +1,35 @@
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
 const defaultState = {
   authenticated: false
 };
 
 const authReducer = (state = defaultState, action) => {
-  // Change code below this line
   switch (action.type) {
-    case 'LOGIN':
+    case LOGIN:
       return {
         authenticated: true
-      };
-    case 'LOGOUT':
+      }
+    case LOGOUT:
       return {
         authenticated: false
-      };
+      }
     default:
       return state;
   }
-  // Change code above this line
 };
 
 const store = Redux.createStore(authReducer);
 
 const loginUser = () => {
   return {
-    type: 'LOGIN'
+    type: LOGIN
   }
 };
 
 const logoutUser = () => {
   return {
-    type: 'LOGOUT'
+    type: LOGOUT
   }
 };
