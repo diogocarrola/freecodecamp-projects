3276f0d71c88df828a53896d81f9001c2fa79e8c
Diogo Carrola <diogo.carrola@gmail.com>
2025-09-01 23:54:33 +0100

    Register a Store Listener

--- front-end-development-libraries/redux/store.js
+++ front-end-development-libraries/redux/store.js
@@ -1,6 +1,30 @@
-const store = Redux.createStore(
-  (state = 5) => state
-);
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
 
 // Change code below this line
-const currentState = store.getState();
+const incrementCount = () => {
+  count++;
+};
+
+store.subscribe(incrementCount);
+// Change code above this line
+
+store.dispatch({type: ADD});
+console.log(count);  // 1
+store.dispatch({type: ADD});
+console.log(count);  // 2
+store.dispatch({type: ADD});
+console.log(count);  // 3
