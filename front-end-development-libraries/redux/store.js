4f446e68726d3f345b143d6c8512f10545ecac68
Diogo Carrola <diogo.carrola@gmail.com>
2025-09-03 23:33:01 +0100

    Remove an Item from an Array

--- front-end-development-libraries/redux/store.js
+++ front-end-development-libraries/redux/store.js
@@ -1,17 +1,20 @@
-const immutableReducer = (state = ['Do not mutate state!'], action) => {
+const immutableReducer = (state = [0,1,2,3,4,5], action) => {
   switch(action.type) {
-    case 'ADD_TO_DO':
+    case 'REMOVE_ITEM':
       // Don't mutate state here or the tests will fail
-      return [...state, action.todo];
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1)
+      ];
     default:
       return state;
   }
 };
 
-const addToDo = (todo) => {
+const removeItem = (index) => {
   return {
-    type: 'ADD_TO_DO',
-    todo
+    type: 'REMOVE_ITEM',
+    index
   }
 }
 
